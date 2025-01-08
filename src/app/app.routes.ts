51642139381755928a311e70958a9e6e09282051
tallys <tallys.prado@gmail.com>
2025-01-08 01:29:05 -0300

    :children_crossing: navegação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ToolbarComponent } from './layout/toolbar/toolbar.component';
+import { ConsultarUsuarioComponent } from './pages/consultar-usuario/consultar-usuario.component';
 
 export const routes: Routes = [
-  {path: '', component: ToolbarComponent},
-  { path: 'login', component: LoginComponent }
+  {path: 'users/create', component: LoginComponent},
+  { path: 'users/search', component: ConsultarUsuarioComponent }
 ];
